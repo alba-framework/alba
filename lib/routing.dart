9c8dc0d49191eb0ef7dd8cbfdbabc76ed268ee49
Mario Juarez <mario@mjp.one>
2022-02-04 17:36:05 +0100

    Export router

--- lib/routing.dart
+++ lib/routing.dart
@@ -2,6 +2,7 @@
 library routing;
 
 export 'src/routing/route.dart';
+export 'src/routing/router.dart';
 export 'src/routing/widgets/listeners.dart';
 export 'src/routing/widgets/router.dart';
 export 'src/routing/widgets/router_root.dart';
